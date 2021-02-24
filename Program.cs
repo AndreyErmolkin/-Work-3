348c5e5e1d3d82289e0a78b9ad3a1088b58bc722
AndreyErmolkin <78263246+AndreyErmolkin@users.noreply.github.com>
2021-02-24 20:16:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HomeWork_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введиту сумму кредита");
+            var summ = Console.ReadLine();
+            var s = double.Parse(summ);
+            Console.WriteLine("Введите значение %");
+            var prosent = Console.ReadLine();
+            var p = float.Parse(prosent);
+            int m;
+            for (m = 0; m < 13; m++) ;
+            {
+                if (m < 13) ;
+                Console.WriteLine("Сумма:" + (s / m + (s - (s - m) * p / 100 / 12)));
+
+            }
+
+
+
+        }
+    }
+
+
+}
